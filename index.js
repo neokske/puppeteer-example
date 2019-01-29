165f7049a7642f6e6ad6c4271bf295ef91e5f0cc
Koen Engels <koen.engels@zinderlabs.com>
2019-01-29 14:33:51 +0100

    :tada: example for puppeteer

---
+++ index.js
@@ -0,0 +1,17 @@
+const puppeteer = require("puppeteer");
+(async () => {
+  const headless = true;
+  const browser = await puppeteer.launch({ headless });
+  const page = await browser.newPage();
+  await page.goto("https://www.google.com", { waitUntil: "networkidle2" });
+  await page.focus(".gLFyf");
+  await page.keyboard.type("javascript");
+  await page.keyboard.press("Enter");
+  await page.waitForNavigation({ waitUntil: "networkidle2" });
+
+  if (headless) {
+    await page.screenshot({ path: "example.png" });
+    console.log("Take a look at example.png");
+    await browser.close();
+  }
+})();
